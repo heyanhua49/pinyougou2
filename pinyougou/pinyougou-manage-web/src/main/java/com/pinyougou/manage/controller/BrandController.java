c4d33970b9c1b8eaeadf3beee254abf2ae2633ae
liaojianbin <meinitcasthavefun@foxmail.com>
2018-10-28 19:06:22 +0800

    品牌列表分页数据加载

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -3,6 +3,7 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.vo.PageResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,19 @@ public class BrandController {
     //从注册中心返回一个代理对象
     @Reference
     private BrandService brandService;
+
+    /**
+     * 根据分页信息查询品牌列表
+     * @param page 页号
+     * @param rows 页大小
+     * @return 品牌列表
+     */
+    @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(defaultValue = "1") Integer page,
+                               @RequestParam(defaultValue = "10") Integer rows){
+        return brandService.findByPage(page, rows);
+    }
+
 
     /**
      * 根据分页信息查询品牌列表
