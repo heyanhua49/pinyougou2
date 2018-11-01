3fdb57c4b17b29e3aae0becbc564b49b5b39ae9d
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-01 18:21:03 +0800

    商家管理系统-配置动态认证类-从数据库中进行数据比对

--- pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/service/impl/UserDetailServiceImpl.java
+++ pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/service/impl/UserDetailServiceImpl.java
@@ -1,5 +1,8 @@
 package com.pinyougou.shop.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +15,8 @@ import java.util.List;
 
 public class UserDetailServiceImpl implements UserDetailsService {
 
+    private SellerService sellerService;
+
     /**
      * 可以在这个方法中根据用户在前端输入的用户名查询、验证用户相关的信息
      * @param username 用户在前端输入的用户名
@@ -20,11 +25,22 @@ public class UserDetailServiceImpl implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //根据用户输入的用户名到数据库中查询对应的商家
+        TbSeller seller = sellerService.findOne(username);
+        if(seller != null && "1".equals(seller.getStatus())){
 
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+            //最正确的做法：应该根据用户id到数据库的角色表查询该用户的角色
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
 
-        //只要用户在前端输入密码为123456都可以登录
-        return new User(username, "123456", authorities);
+            //security将与给定的参数2（密码）与用户输入的密码进行对比；如果一致则认证登录成功，否则失败
+            return new User(username, seller.getPassword(), authorities);
+        }
+
+        return null;
+    }
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
     }
 }
