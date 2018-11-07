232010aa7603aecf0f408252038c6ab4eccbe5ae
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-07 18:36:56 +0800

    门户系统轮播广告缓存加载实现

--- pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -9,6 +9,7 @@ import com.pinyougou.content.service.ContentService;
 import com.pinyougou.service.impl.BaseServiceImpl;
 import com.pinyougou.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
@@ -17,8 +18,14 @@ import java.util.List;
 @Service(interfaceClass = ContentService.class)
 public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements ContentService {
 
+    //内容列表在redis中的key的名称
+    private static final String CONTENT = "content";
+
     @Autowired
     private ContentMapper contentMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public PageResult search(Integer page, Integer rows, TbContent content) {
@@ -41,6 +48,16 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
         List<TbContent> contentList = null;
         //select * from tb_content where `status` = ? and category_id = ? order by sort_order desc
 
+        try {
+            //在查询内容列表的时候先从redis中查询；如果查询到数据则直接返回
+            contentList = (List<TbContent>) redisTemplate.boundHashOps(CONTENT).get(categoryId);
+            if (contentList != null) {
+                return contentList;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Example example = new Example(TbContent.class);
 
         //查询条件对象
@@ -55,6 +72,13 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
         example.orderBy("sortOrder").desc();
 
         contentList = contentMapper.selectByExample(example);
+
+        try {
+            //如果没有查询到则查询mysql中的数据并在返回之前将内容列表数据设置到redis
+            redisTemplate.boundHashOps(CONTENT).put(categoryId, contentList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return contentList;
     }
