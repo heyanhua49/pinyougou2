9989fe7dcadba52c092e50314862c8944b372c57
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-09 16:30:15 +0800

    spring data solr查询测试

--- itcast-solr/src/test/java/cn/itcast/solr/SolrTest.java
+++ itcast-solr/src/test/java/cn/itcast/solr/SolrTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.Criteria;
 import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.ScoredPage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,6 +20,67 @@ public class SolrTest {
 
     @Autowired
     private SolrTemplate solrTemplate;
+
+    //多个条件查询
+    @Test
+    public void multiQuery(){
+        //创建查询条件对象
+        SimpleQuery query = new SimpleQuery();
+        //添加查询条件   Criteria中的构造参数表示在solr中的schema.xml文件中配置的域名称
+        //contains中输入的是查询的关键字；不会分词，如果要分词则可以使用is
+        Criteria criteria = new Criteria("item_title").contains("辣椒");
+        query.addCriteria(criteria);
+
+        //查询价格在大于500小于等于1500
+        Criteria criteria1 = new Criteria("item_price").greaterThan(1500);
+        query.addCriteria(criteria1);
+        Criteria criteria2 = new Criteria("item_price").lessThanEqual(2500);
+        query.addCriteria(criteria2);
+
+        //分页；起始索引号=（页号-1）*页大小
+        //query.setOffset(0);
+        //分页，页大小
+        //query.setRows(10);
+
+        ScoredPage<TbItem> scoredPage = solrTemplate.queryForPage(query, TbItem.class);
+
+        showQueryResult(scoredPage);
+    }
+
+    //单个条件查询
+    @Test
+    public void query(){
+        //创建查询条件对象
+        SimpleQuery query = new SimpleQuery();
+        //添加查询条件   Criteria中的构造参数表示在solr中的schema.xml文件中配置的域名称
+        //contains中输入的是查询的关键字；不会分词，如果要分词则可以使用is
+        Criteria criteria = new Criteria("item_title").contains("辣椒");
+        query.addCriteria(criteria);
+
+        //分页；起始索引号=（页号-1）*页大小
+        query.setOffset(0);
+        //分页，页大小
+        query.setRows(10);
+
+        ScoredPage<TbItem> scoredPage = solrTemplate.queryForPage(query, TbItem.class);
+
+        showQueryResult(scoredPage);
+    }
+
+    /**
+     * 显示查询结果
+     * @param scoredPage 查询结果
+     */
+    private void showQueryResult(ScoredPage<TbItem> scoredPage) {
+        System.out.println("总记录数为：" + scoredPage.getTotalElements());
+        System.out.println("总页数为：" + scoredPage.getTotalPages());
+        for (TbItem tbItem : scoredPage) {
+            System.out.println("id = " + tbItem.getId());
+            System.out.println("title = " + tbItem.getTitle());
+            System.out.println("price = " + tbItem.getPrice());
+            System.out.println("image = " + tbItem.getImage());
+        }
+    }
 
     //根据条件删除
     @Test
