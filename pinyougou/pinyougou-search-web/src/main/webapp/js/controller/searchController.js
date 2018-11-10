338de75ba88c86b14c9ed11b20071855b17675f2
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-10 15:11:47 +0800

    搜索系统-前端过滤条件构建

--- pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,7 +1,7 @@
 app.controller("searchController", function ($scope, searchService) {
 
     //提交到后台的查询条件对象
-    $scope.searchMap = {"keywords":""};
+    $scope.searchMap = {"keywords":"","category":"","brand":"","spec":{}};
 
     //搜索
     $scope.search = function () {
@@ -11,5 +11,29 @@ app.controller("searchController", function ($scope, searchService) {
 
         });
 
+    };
+
+    //添加查询过滤条件
+    $scope.addSearchItem = function (key, value) {
+        if ("category" == key || "brand" == key) {
+            $scope.searchMap[key] = value;
+        } else {
+            //规格
+            $scope.searchMap.spec[key] = value;
+        }
+
+        $scope.search();
+    };
+
+    //从搜索条件导航条中移除过滤条件
+    $scope.removeSearchItem = function (key) {
+        if ("category" == key || "brand" == key) {
+            $scope.searchMap[key] = "";
+        } else {
+            //规格
+            delete $scope.searchMap.spec[key];
+        }
+
+        $scope.search();
     };
 });
