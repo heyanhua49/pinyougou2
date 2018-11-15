6b1d4c6395375b612663755cc3300737f0874b66
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-15 18:33:35 +0800

    用户注册-发送短信验证码

--- pinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/controller/UserController.java
+++ pinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pinyougou.user.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.common.util.PhoneFormatCheckUtils;
 import com.pinyougou.pojo.TbUser;
 import com.pinyougou.user.service.UserService;
 import com.pinyougou.vo.PageResult;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.regex.PatternSyntaxException;
 
 @RequestMapping("/user")
 @RestController
@@ -17,6 +19,26 @@ public class UserController {
 
     @Reference
     private UserService userService;
+
+    /**
+     * 发送手机短信验证码
+     * @param phone 手机号
+     * @return 操作结果
+     */
+    @GetMapping("/sendSmsCode")
+    public Result sendSmsCode(String phone){
+        try {
+            if (PhoneFormatCheckUtils.isPhoneLegal(phone)) {
+                userService.sendSmsCode(phone);
+                return Result.ok("发送短信验证码成");
+            } else {
+                return Result.fail("手机号码格式错误；发送短信验证码失败");
+            }
+        } catch (PatternSyntaxException e) {
+            e.printStackTrace();
+        }
+        return Result.fail("发送短信验证码失败!");
+    }
 
     @RequestMapping("/findAll")
     public List<TbUser> findAll() {
